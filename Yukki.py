24667936d1484f3e30cb611a1727fccddefff582
YukkiBot <84517069+YukkiBot@users.noreply.github.com>
2021-06-29 19:16:27 +0530

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -422,7 +422,7 @@ async def _(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗟𝗲𝗮𝘃𝗲\n\nCommand:\n\n.leave <Channel or Chat ID>"
     if e.sender_id in SMEX_USERS:
         yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if len(e.text) == 7:
+        if len(e.text) > 7:
             bc = yukki[0]
             bc = int(bc)
             text = "Leaving....."
